916b0697e0a774e52beeb8fb480a13610bd7c0d0
juguedes963 <juguedes963@github.com>
2021-02-06 05:13:20 -0300

    batata frita

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/db/db.sqlite3'
+      filename: './src/db/db.sqlite'
     }
     ,migrations:{
       directory:'./src/db/migrations'
